0049ae09826389ec865b8a9785a086389f348131
j_maslak <maslakjules@gmail.com>
2023-06-25 17:03:04 -0500

    Revert "Merge branch 'add_POI_Controller'"
    This reverts commit c4f1a53826383214a4dd9cf48107d608d25b2323, reversing
    changes made to b1c7bc22db9b6f98cfc6a0339dd02db15257bdf3.

---
+++ ARDemo/ARDemo/AboutView.swift
@@ -0,0 +1,32 @@
+//
+//  AboutView.swift
+//  ARDemo
+//
+//  Created by Justin Reini on 4/26/23.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        VStack {
+            Text("AR City Tour")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("Developed by: The Augmenters")
+                .font(.title2)
+                .padding(.bottom)
+            Text("This application was developed by Jackson Atkins, Andrew Brain, Jules Maslak, Jeff Morgan, Justin Reini and Erika Zhou as a senior project for Capstone II at the University of Missouri in Columbia.")
+                .font(.system(size: 18))
+            Spacer()
+        }
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}
---
+++ UIDemo/UIDemo/UIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIDemoApp.swift
+//  UIDemo
+//
+//  Created by Justin Reini on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct UIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ARView()
+        }
+    }
+}

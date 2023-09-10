60c8ca4497fae2a6bada6ac8095136e5f77bef6d
Justin Reini <jr6email@gmail.com>
2023-09-10 18:25:44 -0500

    Added in the AR camera view, changed some icons and added in some default text to showcase object placement

--- CityAssistant/CityAssistant/Views/CityTourView.swift
+++ CityAssistant/CityAssistant/Views/CityTourView.swift
@@ -14,19 +14,45 @@ struct CityTour:
     
     func makeUIView(context: Context) -> ARView {
         
+        // Loads defualt box in
+        // When tapped, launch the POIView screen with appropriate test information
         let arView = ARView(frame: .zero)
+        context.coordinator.view = arView
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        arView.addGestureRecognizer(UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleTap(_:))))
+        let anchor = AnchorEntity()
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        let txt = ModelEntity(mesh: MeshResource.generateText("Test Text", extrusionDepth: 0.01, font: .boldSystemFont(ofSize: 0.01), containerFrame: .zero, alignment: .center,lineBreakMode: .byWordWrapping), materials: [SimpleMaterial(color: .black, isMetallic: true)])
         
+        txt.generateCollisionShapes(recursive: true)
+        
+        anchor.addChild(txt)
+        arView.scene.addAnchor(anchor)
         return arView
         
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+}
+
+class Coordinator: NSObject {
+    weak var view: ARView?
+    
+    @objc func handleTap(_ recognizer: UITapGestureRecognizer) {
+        guard let view = self.view else {return}
+        let tapLocation = recognizer.location(in: view)
+        
+        if let entity = view.entity(at: tapLocation) as? ModelEntity {
+            //POIView(selectedBusiness: CityTourView_Previews.mockPOI, viewModel: CityTourView_Previews.mockViewModel)
+            
+            //Navigation currently not working so for now just registering the tapped object
+            print("clicked")
+        }
+    }
 }
 
 struct CityTourView: View {
@@ -42,14 +68,14 @@ struct CityTourView: View {
     
     init(pois: [POI]) {
         viewModel = CityTourViewModel(pois: pois)
-        poiViewModel = POIViewModel()
+        poiViewModel = POIViewModel(business: CityTourView_Previews.mockPOI)
     }
     
     var body: some View {
         AsyncContentView(source: viewModel, content:  { pois in
             ZStack {
                 if(showARView) {
-                    //CityTour().edgesIgnoringSafeArea(.all)
+                    CityTour().edgesIgnoringSafeArea(.all)
                 }
                 else {
                     POIMapView(pois: pois ?? [],
@@ -73,6 +99,11 @@ struct CityTourView_Previews: PreviewProvider {
     static let mockAddress: Address = Address("101 street st.", "Columbia", "MO", "65201")
     static let mockPOI: POI = POI("Harpo's Columbia", "(417)-111-1111", 4.7, "$$", mockAddress, 3145)
     static let mockPOIs: [POI] = [mockPOI]
+    static let mockReviews: [Review] = [
+        Review("The food here was great. Would recommend bringing family.", 4.5, "Micheal Scott"),
+        Review("Service was slow and waiters were rude. Not worth the price", 2, "Patrick Mahomes"),
+        Review("Good food for a sunny day.", 3, "Tiger Woods")]
+    static let mockViewModel: POIViewModel = POIViewModel(selectedBusiness: mockPOI, reviews: mockReviews)
     
     static var previews: some View {
         CityTourView(pois: mockPOIs)

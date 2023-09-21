273e3ef3fde468ed51fd0e8c2835eaf55f02e2fe
Justin Reini <jr6email@gmail.com>
2023-09-21 17:28:02 -0500

    Bug fixes and tap gestures added

---
+++ CityAssistant/CityAssistant/ViewModels/Tap.swift
@@ -0,0 +1,8 @@
+//
+//  Tap.swift
+//  CityAssistant
+//
+//  Created by Justin Reini on 9/21/23.
+//
+
+import Foundation
---
+++ CityAssistant/CityAssistant/Views/CityARView.swift
@@ -0,0 +1,20 @@
+//
+//  CityARView.swift
+//  CityAssistant
+//
+//  Created by Justin Reini on 9/11/23.
+//
+
+import SwiftUI
+
+struct CityARView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CityARView_Previews: PreviewProvider {
+    static var previews: some View {
+        CityARView()
+    }
+}
